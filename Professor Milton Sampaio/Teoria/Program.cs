ec97f69a3017cdfb3e27fdba011f77c151f78bcf
Lenilson Soares <lenilsonp40@gmail.com>
2024-07-17 13:07:21 -0300

    Array

---
+++ Professor Milton Sampaio/Teoria/Program.cs
@@ -0,0 +1,38 @@
+﻿using Teoria;
+
+//  Parei aula 05
+
+
+//Array
+/*
+ 
+int[] numeros = { 8, 4, 5, 6, 9, 7 };
+numeros.Imprimir(mensagem: "Array inicial");
+
+Console.WriteLine(numeros.Length);
+
+Array.Resize(ref numeros, 20);
+Console.WriteLine($"Novo Tamanho:{numeros.Length}");
+
+Array.Sort(numeros);
+numeros.Imprimir(mensagem: "Array ordenado");
+
+Array.Reverse(numeros);
+numeros.Imprimir("Array invertido");
+
+*/
+
+// Listas - diferente do array a list é dinamica
+
+var numeros = new List<int>() { 8, 5, 1 , 3 , 4 };
+
+Console.WriteLine(value:"Imprimir List");
+numeros.ForEach((item) => Console.WriteLine(item));
+
+var outrosNumeros = new List<int>();
+
+outrosNumeros.Add(2);
+outrosNumeros.Add(5);
+
+
+
---
+++ Professor Milton Sampaio/Teoria/Utils.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Teoria
+{
+    public static class Utils
+    {
+        public static void Imprimir<T>(this T[] array, string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
